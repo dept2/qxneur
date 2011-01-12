aa61425450eacda81770a1d8dbd470328a1d6d11
Alexander Anokhin <anohin@nii.voskhod.ru>
2011-01-12 17:11:31 +0300

    Added roles for applications model

---
+++ src/Config/QXNModelRoles.h
@@ -0,0 +1,13 @@
+#ifndef QXNMODELROLES_H
+#define QXNMODELROLES_H
+
+namespace QXNModelRoles
+{
+  enum Roles
+  {
+    UserRole = Qt::UserRole,
+    ModeEnumRole
+  };
+}
+
+#endif // QXNMODELROLES_H
