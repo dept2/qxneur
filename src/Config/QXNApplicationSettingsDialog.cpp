27380e7f1e4a568aef60b3f5f991e3b161a113e8
Alexander Anokhin <anohin@nii.voskhod.ru>
2011-01-12 17:17:52 +0300

    Renamed classes and widgets

---
+++ src/Config/QXNApplicationSettingsDialog.cpp
@@ -0,0 +1,119 @@
+// Local
+#include "ApplicationSettingsDialog.h"
+
+// Qt
+#include <QX11Info>
+#include <QPushButton>
+
+// Ui
+#include "ui_ApplicationSettingsDialog.h"
+
+// X11
+#include <X11/cursorfont.h>
+#include <X11/Xlib.h>
+#include <X11/Xmu/WinUtil.h>
+
+
+ApplicationSettingsDialog::ApplicationSettingsDialog(QWidget* parent)
+  : QDialog(parent),
+    ui(new Ui::ApplicationSettingsDialog)
+{
+  ui->setupUi(this);
+
+  ui->buttonBox->button(QDialogButtonBox::Ok)->setDisabled(true);
+
+  connect(ui->windowNameEdit, SIGNAL(textChanged(QString)), SLOT(verify()));
+  connect(ui->storeLayoutCheckBox, SIGNAL(toggled(bool)), SLOT(verify()));
+  connect(ui->layoutSwitchingGroup, SIGNAL(toggled(bool)), SLOT(verify()));
+}
+
+
+ApplicationSettingsDialog::~ApplicationSettingsDialog()
+{ }
+
+
+Window selectWindow(Display *dpy)
+{
+  Cursor cursor = XCreateFontCursor(dpy, XC_crosshair);
+
+  Window root = RootWindow(dpy, 0);
+  XGrabPointer(dpy, root, False, ButtonPressMask|ButtonReleaseMask, GrabModeSync, GrabModeAsync, root, cursor, CurrentTime);
+
+  Window target_win = None;
+  int buttons = 0;
+  while ((target_win == None) || (buttons != 0))
+  {
+    XEvent event;
+    XAllowEvents(dpy, SyncPointer, CurrentTime);
+    XWindowEvent(dpy, root, ButtonPressMask|ButtonReleaseMask, &event);
+    switch (event.type)
+    {
+      case ButtonPress:
+        if (target_win == None)
+        {
+          target_win = event.xbutton.subwindow;
+          if (target_win == None)
+            target_win = root;
+        }
+        ++buttons;
+        break;
+      case ButtonRelease:
+        if (buttons > 0)
+          --buttons;
+        break;
+    }
+  }
+
+  XUngrabPointer(dpy, CurrentTime);
+  return XmuClientWindow(dpy, target_win);
+}
+
+
+void ApplicationSettingsDialog::on_windowSelectButton_clicked()
+{
+  Display* display = QX11Info::display();
+  Window window = selectWindow(display);
+
+  XClassHint* classHint = XAllocClassHint();
+
+  if (XGetClassHint(display, window, classHint))
+    ui->windowNameEdit->setText(QString::fromLocal8Bit(classHint->res_class));
+
+  XFree(classHint);
+}
+
+
+void ApplicationSettingsDialog::verify()
+{
+  ui->buttonBox->button(QDialogButtonBox::Ok)->setEnabled(!ui->windowNameEdit->text().isEmpty()
+                                                           && (ui->storeLayoutCheckBox->isChecked()
+                                                               || ui->layoutSwitchingGroup->isChecked()));
+}
+
+
+QString ApplicationSettingsDialog::windowName()
+{
+  return ui->windowNameEdit->text();
+}
+
+
+bool ApplicationSettingsDialog::storeLayout()
+{
+  return ui->storeLayoutCheckBox->isChecked();
+}
+
+
+ApplicationSettingsDialog::LayoutSwitching ApplicationSettingsDialog::layoutSwithching()
+{
+  if (ui->layoutSwitchingGroup->isChecked())
+  {
+    if (ui->forceAutomaticalRadioButton->isChecked())
+      return Automatical;
+    else if (ui->forceManualRadioButton->isChecked())
+      return Manual;
+    else
+      return DontProcess;
+  }
+
+  return Default;
+}
---
+++ src/Config/QXNApplicationsMode.h
@@ -0,0 +1,15 @@
+#ifndef QXNAPPLIACTIONSMODE_H
+#define QXNAPPLIACTIONSMODE_H
+
+namespace LayoutSwitching
+{
+  enum Mode
+  {
+    Default,
+    Manual,
+    Automatical,
+    DontProcess
+  };
+}
+
+#endif // QXNAPPLIACTIONSMODE_H
