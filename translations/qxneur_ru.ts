f7c903c6410e3a642e6a42a19c834b2cbdb7de97
Boris Moiseev <cyberbobs@gmail.com>
2014-11-17 13:03:33 +0300

    Fixed the russian translation

--- translations/qxneur_ru.ts
+++ translations/qxneur_ru.ts
@@ -1,17 +1,333 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU" sourcelanguage="english">
+<TS version="2.0" language="ru_RU" sourcelanguage="en">
+<context>
+    <name>AbbreviationEditDialog</name>
+    <message>
+        <source>Edit abbreviation</source>
+        <translation type="obsolete">Изменение сокращения</translation>
+    </message>
+    <message>
+        <source>Abbreviation:</source>
+        <translation type="obsolete">Сокращение:</translation>
+    </message>
+    <message>
+        <source>Full text:</source>
+        <translation type="obsolete">Полный текст:</translation>
+    </message>
+</context>
+<context>
+    <name>ApplicationSettingsDialog</name>
+    <message>
+        <source>Custom application settings</source>
+        <translation type="obsolete">Настройки приложения</translation>
+    </message>
+    <message>
+        <source>Window name:</source>
+        <translation type="obsolete">Имя окна:</translation>
+    </message>
+    <message>
+        <source>Click to select the application window</source>
+        <translation type="obsolete">Нажмите для выбора окна</translation>
+    </message>
+    <message>
+        <source>Layout switching</source>
+        <translation type="obsolete">Смена раскладки</translation>
+    </message>
+    <message>
+        <source>Don&apos;t process the input for this application</source>
+        <translation type="obsolete">Не обрабатывать ввод для этого приложения</translation>
+    </message>
+    <message>
+        <source>Force automatical layout switching</source>
+        <translation type="obsolete">Всегда переключать раскладку автоматически</translation>
+    </message>
+    <message>
+        <source>Force manual layout switching</source>
+        <translation type="obsolete">Запретить автоматическую смену раскладки</translation>
+    </message>
+    <message>
+        <source>Store layout for the whole application, not window</source>
+        <translation type="obsolete">Одна раскладка для всех окон приложения</translation>
+    </message>
+</context>
+<context>
+    <name>QXNConfig</name>
+    <message>
+        <source>Error initializing xnconfig.</source>
+        <translation type="obsolete">Ошибка при инициализации библиотеки xnconfig.</translation>
+    </message>
+    <message>
+        <source>Error loading config</source>
+        <translation type="obsolete">Ошибка при загрузке файла конфигурации</translation>
+    </message>
+</context>
+<context>
+    <name>QXNConfigDialog</name>
+    <message>
+        <source>General</source>
+        <translation type="obsolete">Общие</translation>
+    </message>
+    <message>
+        <source>Layout</source>
+        <translation type="obsolete">Раскладки</translation>
+    </message>
+    <message>
+        <source>Keyboard shortcuts</source>
+        <translation type="obsolete">Комбинации клавиш</translation>
+    </message>
+    <message>
+        <source>Applications</source>
+        <translation type="obsolete">Приложения</translation>
+    </message>
+    <message>
+        <source>Sounds</source>
+        <translation type="obsolete">Звуки</translation>
+    </message>
+    <message>
+        <source>Abbreviations</source>
+        <translation type="obsolete">Сокращения</translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation type="obsolete">Внешний вид</translation>
+    </message>
+    <message>
+        <source>Work in manual mode</source>
+        <translation type="obsolete">Ручной режим переключения</translation>
+    </message>
+    <message>
+        <source>Self educate</source>
+        <translation type="obsolete">Самообучение</translation>
+    </message>
+    <message>
+        <source>Don&apos;t process word on Enter or Tab</source>
+        <translation type="obsolete">Не проверять слово при нажатии Enter или Tab</translation>
+    </message>
+    <message>
+        <source>Check language during input</source>
+        <translation type="obsolete">Проверять язык при вводе</translation>
+    </message>
+    <message>
+        <source>Log the keyboard input</source>
+        <translation type="obsolete">Сохранять набираемый текст в файл журнала</translation>
+    </message>
+    <message>
+        <source>QXNeur settings</source>
+        <translation type="obsolete">Настройка QXNeur</translation>
+    </message>
+    <message>
+        <source>Correct aCCIDENTAL CapsLock</source>
+        <translation type="obsolete">Исправлять сЛУЧАЙНЫЙ CapsLock</translation>
+    </message>
+    <message>
+        <source>Correct two CApital letters</source>
+        <translation type="obsolete">Исправлять две ПРописные буквы</translation>
+    </message>
+    <message>
+        <source>Correct space with punctuation</source>
+        <translation type="obsolete">Исправлять пробел со знаками препинания</translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation type="obsolete">Дополнительно</translation>
+    </message>
+    <message>
+        <source>Save selection after layout switching</source>
+        <translation type="obsolete">Сохранять выделение после переключения раскладки</translation>
+    </message>
+    <message>
+        <source>Typography</source>
+        <translation type="obsolete">Типографика</translation>
+    </message>
+    <message>
+        <source>Disable CapsLock button</source>
+        <translation type="obsolete">Отключение кнопки CapsLock</translation>
+    </message>
+    <message>
+        <source>Flush internal buffers after Enter or Tab</source>
+        <translation type="obsolete">Сбрасывать при нажатии Enter или Tab</translation>
+    </message>
+    <message>
+        <source>Misc. options</source>
+        <translation type="obsolete">Специальные параметры</translation>
+    </message>
+    <message>
+        <source>Event send delay (ms.):</source>
+        <translation type="obsolete">Задержка перед отправкой события (мс.):</translation>
+    </message>
+    <message>
+        <source>Logging level:</source>
+        <translation type="obsolete">Уровень журналирования:</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="obsolete">Ошибки</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation type="obsolete">Предупреждения</translation>
+    </message>
+    <message>
+        <source>Log</source>
+        <translation type="obsolete">Лог</translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation type="obsolete">Отладка</translation>
+    </message>
+    <message>
+        <source>Trace</source>
+        <translation type="obsolete">Трассировка</translation>
+    </message>
+    <message>
+        <source>Remember layout for every window</source>
+        <translation type="obsolete">Сохранять раскладку для каждого приложения</translation>
+    </message>
+    <message>
+        <source>Custom applications</source>
+        <translation type="obsolete">Исключения</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation type="obsolete">Добавить</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="obsolete">Удалить</translation>
+    </message>
+    <message>
+        <source>Edit...</source>
+        <translation type="obsolete">Правка...</translation>
+    </message>
+    <message>
+        <source>Enable sounds</source>
+        <translation type="obsolete">Включить звуки</translation>
+    </message>
+    <message>
+        <source>Action</source>
+        <translation type="obsolete">Действие</translation>
+    </message>
+    <message>
+        <source>Sound file</source>
+        <translation type="obsolete">Звуковой файл</translation>
+    </message>
+    <message>
+        <source>XNeur start</source>
+        <translation type="obsolete">Запуск XNeur</translation>
+    </message>
+    <message>
+        <source>XNeur reload</source>
+        <translation type="obsolete">Перезапуск XNeur</translation>
+    </message>
+    <message>
+        <source>XNeur stop</source>
+        <translation type="obsolete">Остановка XNeur</translation>
+    </message>
+    <message>
+        <source>Layout 1 key press</source>
+        <translation type="obsolete">Нажатие клавиши в раскладке 1</translation>
+    </message>
+    <message>
+        <source>Layout 2 key press</source>
+        <translation type="obsolete">Нажатие клавиши в раскладке 2</translation>
+    </message>
+    <message>
+        <source>Layout 3 key press</source>
+        <translation type="obsolete">Нажатие клавиши в раскладке 3</translation>
+    </message>
+    <message>
+        <source>Layout 4 key press</source>
+        <translation type="obsolete">Нажатие клавиши в раскладке 4</translation>
+    </message>
+    <message>
+        <source>Layout 1 activated</source>
+        <translation type="obsolete">Включение раскладки 1</translation>
+    </message>
+    <message>
+        <source>Layout 2 activated</source>
+        <translation type="obsolete">Включение раскладки 2</translation>
+    </message>
+    <message>
+        <source>Layout 3 activated</source>
+        <translation type="obsolete">Включение раскладки 3</translation>
+    </message>
+    <message>
+        <source>Layout 4 activated</source>
+        <translation type="obsolete">Включение раскладки 4</translation>
+    </message>
+    <message>
+        <source>Automatic word convert</source>
+        <translation type="obsolete">Автоматическая обработка слова</translation>
+    </message>
+    <message>
+        <source>Manual word convert</source>
+        <translation type="obsolete">Ручная обработка слова</translation>
+    </message>
+    <message>
+        <source>Convert last string</source>
+        <translation type="obsolete">Обработка последней строки</translation>
+    </message>
+    <message>
+        <source>Convert selection</source>
+        <translation type="obsolete">Обработка выделения</translation>
+    </message>
+    <message>
+        <source>Transliterate selection</source>
+        <translation type="obsolete">Транслитерация выделения</translation>
+    </message>
+    <message>
+        <source>Change selection case</source>
+        <translation type="obsolete">Смена регистра выделения</translation>
+    </message>
+    <message>
+        <source>Replace abbreviation</source>
+        <translation type="obsolete">Замена сокращения</translation>
+    </message>
+    <message>
+        <source>Correct aCCIDENTIAL CapsLock</source>
+        <translation type="obsolete">Исправление сЛУЧАЙНОГО Caps Lock</translation>
+    </message>
+    <message>
+        <source>Correct TWo capital letters</source>
+        <translation type="obsolete">Исправление ДВух заглавных букв</translation>
+    </message>
+    <message>
+        <source>Execute user action</source>
+        <translation type="obsolete">Выполнение пользовательского действия</translation>
+    </message>
+    <message>
+        <source>Ignore layout for abbreviations</source>
+        <translation type="obsolete">Игнорировать раскладку для сокращений</translation>
+    </message>
+    <message>
+        <source>Abbreviation</source>
+        <translation type="obsolete">Сокращение</translation>
+    </message>
+    <message>
+        <source>Full text</source>
+        <translation type="obsolete">Полный текст</translation>
+    </message>
+    <message>
+        <source>QXNeur</source>
+        <translation type="obsolete">QXNeur</translation>
+    </message>
+    <message>
+        <source>Abbreviation &quot;%1&quot; already exists. Do you want to replace it?</source>
+        <translation type="obsolete">Сокращение &quot;%1&quot; уже существует. Вы действительно хотите переопределить его?</translation>
+    </message>
+</context>
 <context>
     <name>QXNKeyboard</name>
     <message>
-        <location filename="../src/QXNKeyboard.cpp" line="+24"/>
+        <location filename="../src/QXNKeyboard.cpp" line="+25"/>
         <source>XKB header and library version does not match. The program may work incorrectly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Версия установленной библиотеки XKB не совпадает с версией, использованной при сборке. Приложение может работать некорректно.</translation>
     </message>
     <message>
         <location line="+23"/>
         <source>The X Server does not support a compatible XKB extension.</source>
-        <translation type="unfinished"></translation>
+        <translation>X-сервер не поддерживает расширение XKB.</translation>
     </message>
 </context>
 <context>
@@ -19,103 +335,126 @@
     <message>
         <location filename="../src/QXNLanguage.cpp" line="+103"/>
         <source>Armenian</source>
-        <translation type="unfinished"></translation>
+        <translation>Армянский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Belarusian</source>
-        <translation type="unfinished"></translation>
+        <translation>Белорусский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Bulgarian</source>
-        <translation type="unfinished"></translation>
+        <translation>Болгарский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Czech</source>
-        <translation type="unfinished"></translation>
+        <translation>Чешский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>English</source>
-        <translation type="unfinished"></translation>
+        <translation>Английский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Estonian</source>
-        <translation type="unfinished"></translation>
+        <translation>Эстонский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>French</source>
-        <translation type="unfinished"></translation>
+        <translation>Французский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>German</source>
-        <translation type="unfinished"></translation>
+        <translation>Немецкий</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Kazakh</source>
-        <translation type="unfinished"></translation>
+        <translation>Казахский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Latvian</source>
-        <translation type="unfinished"></translation>
+        <translation>Латвийский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Lithuanian</source>
-        <translation type="unfinished"></translation>
+        <translation>Литовский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Polish</source>
-        <translation type="unfinished"></translation>
+        <translation>Польский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Romanian</source>
-        <translation type="unfinished"></translation>
+        <translation>Румынский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Russian</source>
-        <translation type="unfinished"></translation>
+        <translation>Русский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Spanish</source>
-        <translation type="unfinished"></translation>
+        <translation>Испанский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Ukrainian</source>
-        <translation type="unfinished"></translation>
+        <translation>Украинский</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Неизвестный</translation>
     </message>
 </context>
 <context>
     <name>QXNMenu</name>
     <message>
-        <location filename="../src/QXNMenu.cpp" line="+13"/>
+        <source>Manual mode</source>
+        <translation type="obsolete">Ручной режим</translation>
+    </message>
+    <message>
+        <source>Settings...</source>
+        <translation type="obsolete">Настройки...</translation>
+    </message>
+    <message>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation type="obsolete">Выход</translation>
     </message>
 </context>
 <context>
     <name>QXNTrayIcon</name>
     <message>
-        <location filename="../src/QXNTrayIcon.cpp" line="+34"/>
+        <source>QXNeur: current layout is %1</source>
+        <translation type="obsolete">Текущий язык QXNeur: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/QXNTrayIcon.cpp" line="+46"/>
         <source>Current layout is %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Текущая раскладка: %1</translation>
+    </message>
+    <message>
+        <location line="+35"/>
+        <source>Quit</source>
+        <translation>Выход</translation>
+    </message>
+</context>
+<context>
+    <name>QXNeur</name>
+    <message>
+        <source>Can not start xneur (unknown error)</source>
+        <translation type="obsolete">Невозможно запустить xneur (произошла неизвестная ошибка)</translation>
     </message>
 </context>
 </TS>
